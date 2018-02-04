ec68f96a6f0c69db38e53f5cf0c0f6931906e779
Ashwin Chatterji <ashwinath@hotmail.com>
2018-02-04 17:11:23 +0800

    using rsync instead of scp, added logging

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
-scp -r `pwd` chat@192.168.1.199:/opt/apps/home-dashboard-server/
+rsync -avz -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --progress . chat@192.168.1.199:/opt/apps/home-dashboard-server;
 ssh chat@192.168.1.199 "pm2 restart home-dashboard-server"
--- routes/widgets.js
+++ routes/widgets.js
@@ -3,6 +3,7 @@ const widget_config = require('../config/widgets-config')
 const router = express.Router()
 
 router.get('/', (req, res) => {
+    console.log("requested widgets status")
     res.json(widget_config.widgets)
 })
 
